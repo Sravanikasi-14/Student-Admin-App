2c9ac2fc5224cf57f231047dd3de57fc0faa21cb
Sravani <sravanikasi14@gmail.com>
2026-04-03 12:47:44 +0530

    fixed case issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Student from "./pages/Student";
 import Admin from "./pages/Admin";
 
